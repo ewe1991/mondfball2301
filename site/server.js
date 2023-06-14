22946d20e7897cdd87dbf7c51808d2256e603942
Elliott.Evans <hello@wearebold.co.uk>
2023-06-14 09:02:17 +0100

    update(routes)

--- site/server.js
+++ site/server.js
@@ -118,16 +118,20 @@ class TeamPickerServer {
   }
 
   setupRoutes() {
+    this.app.get("/", (req, res) => {
+      res.sendFile(path.join(__dirname, "dist/index.html"));
+    });
+
     this.app.get("/admin", (req, res) => {
-      res.sendFile(path.join(__dirname, "../site/dist/index.html"));
+      res.sendFile(path.join(__dirname, "dist/index.html"));
     });
 
     this.app.get("/team1", (req, res) => {
-      res.sendFile(path.join(__dirname, "../site/dist/index.html"));
+      res.sendFile(path.join(__dirname, "dist/index.html"));
     });
 
     this.app.get("/team2", (req, res) => {
-      res.sendFile(path.join(__dirname, "../site/dist/index.html"));
+      res.sendFile(path.join(__dirname, "dist/index.html"));
     });
   }
 
